fd992e39bb6b40ab5fed21cc1fa4d9ecc3999aa8
jnoguerapavon <julio.noguera.pavon@gmail.com>
2024-12-26 16:58:43 -0600

    five commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -22,7 +22,7 @@ import { User, UserSchema } from './entities/user.entity';
 
     JwtModule.register({
       global: true,
-      secret: process.env.JWT_SEED,
+      secret: 'Esta_es_la_key_secret',
       signOptions: { expiresIn: '6h' },
     }),
 
